bbea0b49b8722f10a74bfc07635e4a549d952d8e
TheGreatAxios <sawyer@dirtroad.dev>
2025-04-24 12:32:25 -0700

    Remove Unused Region, Fix Paths

--- lib/main.ts
+++ lib/main.ts
@@ -1,132 +1,122 @@
-import { SupportedRegions } from "./regions"
+import { SupportedRegions } from "./regions";
 import { urls } from "./urls";
 
-type SupportedChains = 
-	| "nebula"
-	| "nebula-testnet";
+type SupportedChains = "nebula" | "nebula-testnet";
 
 class PlayerApi {
+    #apiKey: string;
+    #url: string;
 
-	#apiKey: string;
-	#region: SupportedRegions = "us-east-2";
-	#url: string;
+    constructor({
+        region,
+        apiKey,
+    }: {
+        region: SupportedRegions;
+        apiKey: string;
+    }) {
+        this.#apiKey = apiKey;
+        this.#url = urls[region];
+    }
 
-	constructor({
-		region,
-		apiKey
-	}: {
-		region: SupportedRegions
-		apiKey: string;
-	}) {
-		this.#apiKey = apiKey;
-		this.#region = region;
-		this.#url = urls[region];
-	}
+    public changeRegion(region: SupportedRegions) {
+        this.#url = urls[region];
+    }
 
-	public changeRegion(region: SupportedRegions) {
-		this.#region = region;
-		this.#url = urls[region];
-	}
+    public setApiKey(apiKey: string) {
+        this.#apiKey = apiKey;
+    }
 
-	public setApiKey(apiKey: string) {
-		this.#apiKey = apiKey;
-	}
+    public async createPlayer({
+        chainName,
+        suppliedId,
+    }: {
+        chainName: SupportedChains;
+        suppliedId: string;
+    }) {
+        const response = await fetch(`${this.#url}/v1/players`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": this.#apiKey,
+            },
+            body: JSON.stringify({
+                chainName,
+                suppliedId,
+            }),
+        });
 
-	public async createPlayer({
-		chainName,
-		suppliedId
-	  }: {
-		chainName: SupportedChains,
-		suppliedId: string
-	  }) {
-		const response = await fetch(`${this.#url}/v1/players`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				"x-api-key": this.#apiKey
-			},
-			body: JSON.stringify({
-				chainName,
-				suppliedId
-			})
-		});
+        return await response.json();
+    }
 
-		return await response.json();
-	  }
+    public async sendTransaction({
+        chainName,
+        data,
+        playerId,
+        value,
+        to,
+    }: {
+        chainName: SupportedChains;
+        playerId: string;
+        data: `0x${string}`;
+        value: number | string;
+        to: `0x${string}`;
+    }) {
+        const response = await fetch(`${this.#url}/v1/transactions`, {
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": this.#apiKey,
+            },
+            body: JSON.stringify({
+                chainName,
+                data,
+                playerId,
+                value,
+                to,
+            }),
+        });
 
-	  public async sendTransaction({
-		chainName,
-		data,
-		playerId,
-		value,
-		to
-	  }: {
-		chainName: SupportedChains,
-		playerId: string,
-		data: `0x${string}`,
-		value: number | string,
-		to: `0x${string}`
-	  }) {
-		const response = await fetch(`${this.#url}/v1/transactions`, {
-			headers: {
-				"Content-Type": "application/json",
-				"x-api-key": this.#apiKey
-			},
-			body: JSON.stringify({
-				chainName,
-				data,
-				playerId,
-				value,
-				to
-			})
-		});
+        return await response.json();
+    }
 
-		return await response.json();
-	  }
+    public async estimateGas({
+        chainName,
+        data,
+        playerId,
+        value,
+        to,
+    }: {
+        chainName: SupportedChains;
+        playerId: string;
+        data: `0x${string}`;
+        value: number | string;
+        to: `0x${string}`;
+    }) {
+        const response = await fetch(`${this.#url}/v1/transactions/estimate`, {
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": this.#apiKey,
+            },
+            body: JSON.stringify({
+                chainName,
+                data,
+                playerId,
+                value,
+                to,
+            }),
+        });
 
-	  public async estimateGas({
-		chainName,
-		data,
-		playerId,
-		value,
-		to
-	  }: {
-		chainName: SupportedChains,
-		playerId: string,
-		data: `0x${string}`,
-		value: number | string,
-		to: `0x${string}`
-	  }) {
-		const response = await fetch(`${this.#url}/v1/transactions/estimate`, {
-			headers: {
-				"Content-Type": "application/json",
-				"x-api-key": this.#apiKey
-			},
-			body: JSON.stringify({
-				chainName,
-				data,
-				playerId,
-				value,
-				to
-			})
-		});
+        return await response.json();
+    }
 
-		return await response.json();
-	  }
-
-	  public async getPlayer({
-		playerId,
-	  }: {
-		playerId: string
-	  }) {
-		const response = await fetch(`${this.#url}/v1/players/${playerId}`, {
-			headers: {
-				"Content-Type": "application/json",
-				"x-api-key": this.#apiKey
-			}
-		});
-		return await response.json();
-	  }
+    public async getPlayer({ playerId }: { playerId: string }) {
+        const response = await fetch(`${this.#url}/v1/players/${playerId}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": this.#apiKey,
+            },
+        });
+        return await response.json();
+    }
 }
 
-export { PlayerApi }
+export { PlayerApi };
